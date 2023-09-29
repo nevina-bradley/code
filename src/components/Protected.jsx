69266e2835ec02f5986f287b4e93fc7f9c267336
nevina-bradley <129434576+nevina-bradley@users.noreply.github.com>
2023-09-29 11:53:16 -0400

    Add files via upload

---
+++ src/components/Protected.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect, useRef } from 'react';
+import axios from "axios";
+
+const Protected = ({ token }) => {
+  const isRun = useRef(false);
+
+  useEffect(() => {
+    if (isRun.current) return;
+
+    isRun.current = true;
+
+    const config = {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    };
+
+    axios.get("/documents", config).then((res) => console.log(res.data)).catch((err) => console.error(err));
+  }, []);
+
+  return data ? (
+    <>
+      {data.map((rec, i) => (
+        <h3 key={i}>`${rec}`</h3>
+      ))}
+    </>
+  ) : (
+    <div>Protected</div>
+  );
+};
+
+export default Protected;
